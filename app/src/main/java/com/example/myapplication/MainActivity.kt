225df8c296397c4b406c43b7188f7be15e9bc5e4
Ricardo Jaciel Loza Chavez <20243ds179@utez.com>
2025-12-09 17:44:57 -0600

    Organizacion MVVM

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import com.example.myapplication.ui.login.LoginScreen
+import com.example.myapplication.ui.screens.LoginScreen
 import com.example.myapplication.ui.home.HomeScreen
 
 class MainActivity : ComponentActivity() {
