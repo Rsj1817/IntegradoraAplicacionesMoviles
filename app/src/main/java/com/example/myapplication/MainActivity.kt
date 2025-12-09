b6751342cf53476f639193829533bd1b3b4946dd
Rsj1817 <20243ds185@utez.edu.mx>
2025-12-09 17:43:03 -0600

    Ediciones 4

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -38,7 +39,9 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                         composable("home") {
-                            HomeScreen(onExit = { navController.popBackStack() })
+                            HomeScreen(
+                                onExit = { navController.popBackStack() }
+                            )
                         }
                     }
                 }
