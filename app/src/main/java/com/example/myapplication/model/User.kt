96a6c6a237ec898eaa676fb9bb356931a7cf07bd
ricardojaciellozachavez <20243ds179@utez.edu.mx>
2025-12-10 14:00:37 -0600

    Realize User

--- app/src/main/java/com/example/myapplication/model/User.kt
+++ app/src/main/java/com/example/myapplication/model/User.kt
@@ -1,2 +1,12 @@
 package com.example.myapplication.model
 
+/**
+ * Modelo simple de usuario.
+ * Podemos extenderlo con más campos que tu backend devuelva (avatarUrl, roles, etc.)
+ */
+data class User(
+    val id: Long = 0L,
+    val username: String = "",
+    val email: String? = null,
+    val token: String? = null
+)
